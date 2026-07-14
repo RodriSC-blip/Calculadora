224c34fb4b9cfb406ffdb685c6d643e28826fe55
VAIO <ryanrodrigo880@gmail.com>
2026-07-14 20:54:58 -0300

    1º Commit

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 valor = int(input("Digite um valor: "))
-valor2 = int(input("Digite outro valor: "))
-resultado = valor + valor2
-print(resultado)
+
+for valor in range(valor + 1):
+    print(" ")
+    print( "TABUADA DA ABELINHA:")
+
+    for i in range(1, 11):
+        print(f"{valor} x {i} = {valor*i}")
+
+print(" ")
